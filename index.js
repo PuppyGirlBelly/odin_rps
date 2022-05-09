d9f01536a2b81082bc0d20ade5342323dc251b73
SoftAnnaLee <jazzchamp600@gmail.com>
2022-05-09 16:33:12 -0600

    Formatting

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
 function computerPlay() {
-    const play = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random() * play.length); 
-    return play[random];
+  const play = ["rock", "paper", "scissors"];
+  const random = Math.floor(Math.random() * play.length);
+  return play[random];
 }
 
 function playRound(playerSelection, computerSelection) {
-    switch (playerSelection) {
-        case "r": // Rock
-            switch (computerSelection) {
-                case "rock":
-                    return "Tie!";
-                case "paper":
-                    return "Paper beats rock! You lose!";
-                case "scissors":
-                    return "Rock beats scissors! You win!";
-            }
-        case "p": // Paper
-            switch (computerSelection) {
-                case "rock":
-                    return "Paper beats rock! You win!";
-                case "paper":
-                    return "Tie!";
-                case "scissors":
-                    return "Scissors beats paper! You lose!";
-            }
-        case "s": // Scissors
-            switch (computerSelection) {
-                case "rock":
-                    return "Rock beats scissors! You lose!";
-                case "paper":
-                    return "Scissors beats paper! You win!";
-                case "scissors":
-                    return "Tie!";
-            }
-    }
+  switch (playerSelection) {
+    case "r": // Rock
+      switch (computerSelection) {
+        case "rock":
+          return "Tie!";
+        case "paper":
+          return "Paper beats rock! You lose!";
+        case "scissors":
+          return "Rock beats scissors! You win!";
+      }
+    case "p": // Paper
+      switch (computerSelection) {
+        case "rock":
+          return "Paper beats rock! You win!";
+        case "paper":
+          return "Tie!";
+        case "scissors":
+          return "Scissors beats paper! You lose!";
+      }
+    case "s": // Scissors
+      switch (computerSelection) {
+        case "rock":
+          return "Rock beats scissors! You lose!";
+        case "paper":
+          return "Scissors beats paper! You win!";
+        case "scissors":
+          return "Tie!";
+      }
+  }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let playerSelection, computerSelection, result;
+  let playerScore = 0;
+  let computerScore = 0;
+  let playerSelection, computerSelection, result;
 
-    for (var i = 0; i < 5; i++){
-        playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors': ");
-        playerSelection = playerSelection.toLowerCase()[0];
-        computerSelection = computerPlay();
+  for (var i = 0; i < 5; i++){
+    playerSelection = prompt("Enter 'Rock', 'Paper', or 'Scissors': ");
+    playerSelection = playerSelection.toLowerCase()[0];
+    computerSelection = computerPlay();
 
-        result = playRound(playerSelection, computerSelection);
+    result = playRound(playerSelection, computerSelection);
 
-        if (result.includes("win")) {
-            playerScore++;
-        } else if (result.includes("lose")) {
-            computerScore++;
-        }
-
-        console.log(`${result}\nPlayer: ${playerScore}\nComputer: ${computerScore} `)
+    if (result.includes("win")) {
+      playerScore++;
+    } else if (result.includes("lose")) {
+      computerScore++;
     }
+
+    console.log(`
+      ${result}\nPlayer: ${playerScore}\nComputer: ${computerScore}
+    `)
+  }
 }
