96344c33bd10e171a8b3b6c7728570debd69a751
SoftAnnaLee <jazzchamp600@gmail.com>
2022-05-09 15:12:11 -0600

    Add playRound() function

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ function computerPlay() {
     const random = Math.floor(Math.random() * play.length); 
     return play[random];
 }
+
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "r": // Rock
+            switch (computerSelection) {
+                case "rock":
+                    return "Tie!";
+                case "paper":
+                    return "Paper beats rock! You lose!";
+                case "scissors":
+                    return "Rock beats scissors! You win!";
+            }
+        case "p": // Paper
+            switch (computerSelection) {
+                case "rock":
+                    return "Paper beats rock! You win!";
+                case "paper":
+                    return "Tie!";
+                case "scissors":
+                    return "Scissors beats paper! You lose!";
+            }
+        case "s": // Scissors
+            switch (computerSelection) {
+                case "rock":
+                    return "Rock beats scissors! You lose!";
+                case "paper":
+                    return "Scissors beats paper! You win!";
+                case "scissors":
+                    return "Tie!";
+            }
+    }
+}
